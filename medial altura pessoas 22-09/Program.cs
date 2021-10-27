0b8145d46a360dc04f2171c9b5c455c629ef01bd
edumartimi <eduardomartimiano@hotmail.com>
2021-10-27 16:59:16 -0300

    Adicionar arquivos de projeto.

---
+++ medial altura pessoas 22-09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace medial_altura_pessoas_22_09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criando a variável de entrada
+            Console.WriteLine("Quantas pessoas serão medidas?");
+            int n = int.Parse(Console.ReadLine());
+            //Criando vetor com o número da variável de entrada
+            double[] vect = new double[n];
+            //Utilização do comando for para percorrer o vetor
+            for (int i = 0; i < n; i++)
+            {
+                int numpessoa = i + 1;
+                //Entrada dos elementos no vetor pelo usuário
+                Console.WriteLine("A altura da pessoa " + numpessoa + " é de:");
+                vect[i] = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+            }
+            //Variável soma       
+            double sum = 0.0;
+            //Utilização do for para percorrer o vetor
+            for (int i = 0; i < n; i++)
+            {
+                //Somatoria dos elementos inseridos
+                sum += vect[i]; 
+            }
+            double media = sum / vect.Length;
+            Console.WriteLine("a media das alturas é :" + media);
+        }
+    }
+
+}
